8edf2609a3c014035d4e164d2d23e14b59bccc14
YUNA0222 <gold8077@gmail.com>
2026-04-05 21:39:10 +0900

    Feat: auth 전용 페이지 구성

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import classNames from "classnames/bind";
+
+import styles from "./page.module.css";
+
+const cx = classNames.bind(styles);
+
+export default function Sginup() {
+  return <div className={cx("content")}>{}</div>;
+}
