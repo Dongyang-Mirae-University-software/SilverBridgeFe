90b6d6ec66e0ebef14bf5da2743571f7dbb64951
YUNA0222 <gold8077@gmail.com>
2026-04-05 21:37:44 +0900

    Feat: 임시 헤더 추가

---
+++ src/app/_component/Header.tsx
@@ -0,0 +1,9 @@
+import classNames from "classnames/bind";
+
+import styles from "./Header.module.css";
+
+const cx = classNames.bind(styles);
+
+export default function Header() {
+  return <header>Header</header>;
+}
