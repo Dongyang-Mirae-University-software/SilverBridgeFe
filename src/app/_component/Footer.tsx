39d191368a8799c4b2d68cb15c624bea3ca1c3dd
YUNA0222 <gold8077@gmail.com>
2026-04-05 21:37:11 +0900

    Feat:  임시 푸터 추가

---
+++ src/app/_component/Footer.tsx
@@ -0,0 +1,9 @@
+import classNames from "classnames/bind";
+
+import styles from "./Footer.module.css";
+
+const cx = classNames.bind(styles);
+
+export default function Footer() {
+  return <footer>footer</footer>;
+}
