78596775a4e2caaa073fb23339aaa90548b2293b
YUNA0222 <gold8077@gmail.com>
2026-04-08 20:57:23 +0900

    freat: 라디오 버튼 기본 설정

--- src/app/(auth)/signup/_component/SignupForm.tsx
+++ src/app/(auth)/signup/_component/SignupForm.tsx
@@ -26,10 +26,9 @@ export default function SignupForm() {
     <form className={cx('container')} onSubmit={onSubmit}>
       <TextInput
         label={'이메일'}
-        placeholder={'입력'}
+        placeholder={'이메일을 입력 해주세요'}
         required
         {...register('email', emailRules('에러요'))}
-        isArrow={false}
         error={Boolean(errors.email && allValues.email && allValues.email.trim() !== '')}
         errorText={errors.email?.message}
       />
@@ -38,7 +37,6 @@ export default function SignupForm() {
         placeholder={'입력'}
         required
         {...register('password', passwordRules('에러요'))}
-        isArrow={false}
         error={Boolean(errors.password && allValues.password && allValues.password.trim() !== '')}
         errorText={errors.password?.message}
       />
@@ -47,7 +45,6 @@ export default function SignupForm() {
         placeholder={'입력'}
         required
         {...register('passwordCheck', passwordCheckRules('에러요'))}
-        isArrow={false}
         error={Boolean(errors.passwordCheck && allValues.passwordCheck && allValues.passwordCheck.trim() !== '')}
         errorText={errors.passwordCheck?.message}
       />
@@ -56,7 +53,6 @@ export default function SignupForm() {
         placeholder={'입력'}
         required
         {...register('name', textRules('에러요', 2))}
-        isArrow={false}
         error={Boolean(errors.name && allValues.name && allValues.name.trim() !== '')}
         errorText={errors.name?.message}
       />
@@ -65,13 +61,12 @@ export default function SignupForm() {
         placeholder={'입력'}
         required
         {...register('phone', phoneRules('에러요'))}
-        isArrow={false}
         error={Boolean(errors.phone && allValues.phone && allValues.phone.trim() !== '')}
         errorText={errors.phone?.message}
       />
       <div>
         <label className={cx('radio')} htmlFor="WARD">
-          <input id="WARD" type="radio" value="WARD" {...register('role')} />
+          <input id="WARD" type="radio" value="WARD" {...register('role')} defaultChecked />
           <span>노인</span>
         </label>
         <label className={cx('radio')} htmlFor="guardian">
@@ -80,7 +75,7 @@ export default function SignupForm() {
         </label>
       </div>
 
-      <button className={cx('button')} type="submit">
+      <button className={cx('button')} disabled={!isValid} type="submit">
         전송
       </button>
     </form>
