0a9a7ed491eb3d235b5ff128985a7480f31a4751
YUNA0222 <gold8077@gmail.com>
2026-04-08 20:22:29 +0900

    feat: config 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
