8a7983c3461608a028974ca899506f08c78ba2b5
YUNA0222 <gold8077@gmail.com>
2026-04-09 12:08:31 +0900

    Feat: AuthTimer 추가

---
+++ src/app/(auth)/_components/AuthTimer.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames/bind';
+import styles from './AuthTimer.module.css';
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  time: string;
+  isExpired: boolean;
+  content?: string;
+}
+
+export default function AuthTimer({ time, isExpired, content }: Props) {
+  return (
+    <div>
+      {content && <p>{content}</p>}
+      <span>{time}</span>
+      {isExpired && <span className={cx('expired')}>시간 만료</span>}
+    </div>
+  );
+}
