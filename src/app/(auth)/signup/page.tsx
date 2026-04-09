0a18f8e6a7d2fcdd1d3388c26d7557d057843f74
YUNA0222 <gold8077@gmail.com>
2026-04-09 12:11:20 +0900

    Design: 돔구조 변경

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,14 +1,5 @@
-import classNames from 'classnames/bind';
-
-import styles from './page.module.css';
 import SignupContent from './_component/SignupContent';
 
-const cx = classNames.bind(styles);
-
 export default function Sginup() {
-  return (
-    <div className={cx('signup-wrap')}>
-      <SignupContent />
-    </div>
-  );
+  return <SignupContent />;
 }
