5b702a5c0d42e0a3b8288681bbba2f787ddac6be
YUNA0222 <gold8077@gmail.com>
2026-04-09 12:11:05 +0900

    Feat: 이메일 인증 및 검사 api 설정

--- src/service/api/auth.ts
+++ src/service/api/auth.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { ISignupReq } from '../interface/auth';
+import { IEmailVerityReq, ISignupReq } from '../interface/auth';
 import { CommonResponse } from '../interface/common';
 
-const AUTH_API_PATH = '/api';
+const AUTH_API_PATH = '/api/auth';
 
 export async function signup(body: ISignupReq) {
   const res = await axios.post<CommonResponse<null>>(`${AUTH_API_PATH}/register`, body).then(res => res.data);
@@ -10,4 +10,14 @@ export async function signup(body: ISignupReq) {
   return res.data;
 }
 
-export default signup;
+export async function eamaillSend(body: { email: string }) {
+  const res = await axios.post<CommonResponse<null>>(`${AUTH_API_PATH}/email/send`, body).then(res => res.data);
+
+  return res.data;
+}
+
+export async function emailVerify(body: IEmailVerityReq) {
+  const res = await axios.post<CommonResponse<null>>(`${AUTH_API_PATH}/email/verify`, body).then(res => res.data);
+
+  return res.data;
+}
