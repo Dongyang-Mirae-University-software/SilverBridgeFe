84a8c0788c4c05f333785e9948a7cbb82b871d2f
YUNA0222 <gold8077@gmail.com>
2026-04-09 12:09:25 +0900

    Feat: 사용자 데이터 공유

--- src/app/(auth)/signup/_component/SignupContent.tsx
+++ src/app/(auth)/signup/_component/SignupContent.tsx
@@ -6,24 +6,26 @@ import styles from './SignupContent.module.css';
 import SignupForm from './SignupForm';
 import { useState } from 'react';
 import EmailVerification from './EmailVerification';
-import SignupComplete from './SignupComplete';
+import useSignupForm from '@/app/_hook/useSignupForm';
+import Step from '../../_components/Step';
 
 const cx = classNames.bind(styles);
+const STEP_LABELS = ['정보 입력', '이메일 인증', '가입 완료'];
 
 export default function SignupContent() {
   const [step, setStep] = useState<number>(2);
+  const signupForm = useSignupForm();
 
   return (
     <div className={cx('sign-wrap')}>
       <div>
         <h1> 회원가입</h1>
-        {/* TODO: step구현  */}
-        <span>step</span>
+        <Step step={step} stepList={STEP_LABELS} />
       </div>
       <div className={cx('contnet')}>
-        {step === 1 && <SignupForm onNext={() => setStep(2)} />}
-        {step === 2 && <EmailVerification />}
-        {step === 3 && <SignupComplete />}
+        {step === 1 && <SignupForm signupForm={signupForm} onNext={() => setStep(2)} />}
+        {step === 2 && <EmailVerification signupForm={signupForm} />}
+        {/* {step === 3 && <SignupComplete />} */}
       </div>
     </div>
   );
