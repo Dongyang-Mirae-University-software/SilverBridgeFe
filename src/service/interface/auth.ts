7bfebe01064cde9fa5a2a245fb4a9a3f807980b6
YUNA0222 <gold8077@gmail.com>
2026-04-25 23:26:43 +0900

    feat: 카카오 로그인/회원가입 인터페이스 추가
    - IKakaoSigninReq, IKakaoSigninRes 인터페이스 추가
    - IKakaoSignupReq, IKakaoSignupRes 인터페이스 추가

--- src/service/interface/auth.ts
+++ src/service/interface/auth.ts
@@ -48,3 +48,46 @@ export interface IPasswordResetReq {
 export interface IFindPasswordTokenResponse {
   token: string;
 }
+
+export interface IKakaoSigninReq {
+  code: string;
+}
+
+export interface IKakaoSigninRes {
+  success: boolean;
+  message: string;
+  data: {
+    kakaoId?: string;
+    email?: string;
+    name?: string;
+    profileImageUrl?: string;
+    accessToken?: string;
+    refreshToken?: string;
+    userId?: string;
+    role?: string;
+    newUser: boolean;
+  }
+}
+
+export interface IKakaoSignupReq {
+  kakaoId: string;
+  name: string;
+  phone: string;
+  role: RoleType;
+  profileImageUrl?: string;
+  address: string;
+  addressDetail: string;
+}
+
+export interface IKakaoSignupRes {
+  success: boolean;
+  message: string;
+  data: {
+    accessToken: string;
+    refreshToken: string;
+    userId: string;
+    email: string;
+    name: string;
+    role: string;
+  }
+}
