359e51285a126a97d6beec84278e6cab64789795
YUNA0222 <gold8077@gmail.com>
2026-04-27 09:04:46 +0900

    refactor(auth): separate kakao signup into KakaoSignupForm and normal SignupForm

--- src/app/(auth)/signup/_components/SignupContent.tsx
+++ src/app/(auth)/signup/_components/SignupContent.tsx
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 
 import styles from './SignupContent.module.css';
 import SignupForm from './SignupForm';
+import KakaoSignupForm from './KakaoSignupForm';
 import { useRouter } from 'next/navigation';
 
 const cx = classNames.bind(styles);
@@ -41,7 +42,7 @@ export default function SignupContent({ searchParams }: SignupContentProps) {
         <h1> 회원가입</h1>
       </div>
       <div className={cx('contnet')}>
-        <SignupForm isKakao={isKakao} kakaoData={kakaoData} />
+        {isKakao && kakaoData ? <KakaoSignupForm kakaoData={kakaoData} /> : <SignupForm />}
       </div>
       <button onClick={() => router.push('/login')}>뒤로 가기</button>
     </div>
