adcf865934825f3ca6efd7e9d4c0e6947ae24fb8
YUNA0222 <gold8077@gmail.com>
2026-06-11 20:46:28 +0900

    feat: SVG 아이콘 파일 추가 및 SVGR 설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS?.split(','),
   devIndicators: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [{ loader: '@svgr/webpack', options: { typescript: false, dimensions: false } }],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
