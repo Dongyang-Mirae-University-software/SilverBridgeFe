2e10bb5c0ffa419d8d784387ef1be0002cbd7dec
yuna0222 <gold8077@gmail.com>
2026-09-01 18:42:17 +0900

    Remove: 불필요한 로직 제거

--- src/app/(guardian)/guardian/settings/page.tsx
+++ src/app/(guardian)/guardian/settings/page.tsx
@@ -1,14 +1,10 @@
-import { Suspense } from 'react';
-
 import PageLayout from '@/components/layout/PageLayout';
 import GuardianSettingsContent from './_components/GuardianSettingsContent';
 
 export default function GuardianSettingsPage() {
   return (
     <PageLayout title="환경설정" description="알림 설정과 계정을 관리합니다.">
-      <Suspense>
-        <GuardianSettingsContent />
-      </Suspense>
+      <GuardianSettingsContent />
     </PageLayout>
   );
 }
